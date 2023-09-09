2dfa85a44ebf5760f90caad07c2f693e0587f794
Vivek <365anu65@gmail.com>
2023-09-09 22:44:14 +0530

    error resolve on community page..

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -10,19 +10,19 @@ export interface Review {
 
 const reviews: Review[] = [
   {
-    text: "Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts to any design, and the build size is tiny.",
+    text: "Tailwind CSS is the only framework that I've seen scale on large teams. Its easy to customize, adapts to any design, and the build size is tiny.",
     author: "Sarah Dayan",
     role: "Staff Engineer, Algolia",
     imageSrc: "https://wallpapercave.com/wp/wp5090673.jpg",
   },
   {
-    text: "Tailwind CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts to any design, and the build size is tiny.",
+    text: "Tailwind CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. Its easy to customize, adapts to any design, and the build size is tiny.",
     author: "Sarah Dayan",
     role: "Staff Engineer, Algolia",
     imageSrc: "https://wallpapercave.com/wp/wp5090673.jpg",
   },
   {
-    text: "Tailwind CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts to any design, and the build size is tiny.",
+    text: "Tailwind CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. nd CSS is the only framework that I've seen scale on large teams. Its easy to customize, adapts to any design, and the build size is tiny.",
     author: "Sarah Dayan",
     role: "Staff Engineer, Algolia",
     imageSrc: "https://wallpapercave.com/wp/wp5090673.jpg",
@@ -35,14 +35,14 @@ const reviews: Review[] = [
       "https://pbs.twimg.com/profile_images/1218796685658218496/1ZL6aLyP_400x400.jpg",
   },
   {
-    text: "Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts to any design, and the build size is tiny.",
+    text: "Tailwind CSS is the only framework that I've seen scale on large teams. Its easy to customize, adapts to any design, and the build size is tiny.",
     author: "Sarah Dayan",
     role: "Staff Engineer, Algolia",
     imageSrc:
       "https://pbs.twimg.com/profile_images/1218796685658218496/1ZL6aLyP_400x400.jpg",
   },
   {
-    text: "Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts to any design, and the build size is tiny.",
+    text: "Tailwind CSS is the only framework that I've seen scale on large teams. Its easy to customize, adapts to any design, and the build size is tiny.",
     author: "Sarah Dayan",
     role: "Staff Engineer, Algolia",
     imageSrc:
@@ -56,7 +56,7 @@ const ReviewsPage: React.FC = () => {
       <div className="text-center">
         <h1 className="text-3xl font-semibold mb-2">Reviews</h1>
         <p className="text-lg text-gray-600 mb-8">
-          Here's what we hear about sunshine:
+          Here is what we hear about sunshine:
         </p>
       </div>
       <div className="columns-1 md:columns-2 xl:columns-3">
